9161c785c3be273841a54031a5d2bb6e62caca33
korea-in <rudals2392@gmail.com>
2025-08-29 13:10:20 +0900

    장애물 자동 생성로직 개발

--- main.py
+++ main.py
@@ -1,47 +1,79 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QLabel
 from random import randint
 
-WIDTH_SIZE = 20
-HEIGHT_SIZE = 20
-
+WIDTH_SIZE = 30
+HEIGHT_SIZE = 30
 class MyApp(QWidget):
     def __init__(self):
         super().__init__()
-        self.buttons = [[None for _ in range(WIDTH_SIZE)] for _ in range(HEIGHT_SIZE)]
+        self.boxes = [[None for _ in range(WIDTH_SIZE)] for _ in range(HEIGHT_SIZE)]
         self.initUI()
 
     def initUI(self):
         grid = QGridLayout()
-        grid.setSpacing(0)                 # 버튼 간격 0
+        grid.setSpacing(0)                  # 박스 간격 0
         grid.setContentsMargins(0, 0, 0, 0) # 바깥 여백 0
         self.setLayout(grid)
 
         for h in range(HEIGHT_SIZE):
             for w in range(WIDTH_SIZE):
-                btn = QPushButton(self)
-                btn.setFixedSize(20, 20)   # 크기 고정
-                self.buttons[h][w] = btn
-                grid.addWidget(btn, h, w)
+                label = QLabel(self)
+                label.setFixedSize(13, 13)    # 크기 고정
+                label.setStyleSheet("background-color: #eeeeee;")
+                self.boxes[h][w] = label
+                grid.addWidget(label, h, w)
 
         self.setWindowTitle('QGridLayout')
         self.show()
+    
+    def updateMap(self, map_array):
+        for h in range(HEIGHT_SIZE):
+            for w in range(WIDTH_SIZE):
+                if(map_array[h][w] == 1): #시작지점
+                    self.boxes[h][w].setStyleSheet("background-color: #d84141;")
+                elif(map_array[h][w] == 2): #도착지점
+                    self.boxes[h][w].setStyleSheet("background-color: #4b41d8;")
+                elif(map_array[h][w] ==  3): #탐색중
+                    self.boxes[h][w].setStyleSheet("background-color: #aaaaaa;")
+                elif(map_array[h][w] ==  4): #탐색완료
+                    self.boxes[h][w].setStyleSheet("background-color: #aaaaaa;")
+                elif(map_array[h][w] ==  9): #장애물
+                    self.boxes[h][w].setStyleSheet("background-color: #111111;")
 
 class Dot():
-    def __init__(self):
-      self.x = randint(0, WIDTH_SIZE-1)
-      self.y = randint(0, HEIGHT_SIZE-1)
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-def changeColor(color: str, qbutton: QPushButton):
-    qbutton.setStyleSheet(f"background-color: {color};")
+def changeColor(color: str, qlabel: QLabel):
+    qlabel.setStyleSheet(f"background-color: {color};")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyApp()
-    unit = Dot()
-    destination = Dot()
+
+    map_array = [[0 for _ in range(WIDTH_SIZE)] for _ in range(HEIGHT_SIZE)]
     
-    changeColor("#d84141", ex.buttons[unit.y][unit.x])
-    changeColor("#4b41d8", ex.buttons[destination.y][destination.x])
+    # 시작지점과 종료지점을 랜덤으로 생성
+    map_array[0][0] = 1
+    map_array[HEIGHT_SIZE-1][WIDTH_SIZE-1] = 2
+    
+    # 장애물 랜덤으로 생성
+    for i in range(20):
+        obstacle_width = randint(2, 4)
+        obstacle_height = randint(2, 4)
+        obstacle_x = randint(0, WIDTH_SIZE-1)
+        obstacle_y = randint(0, HEIGHT_SIZE-1)
+        for j in range(obstacle_height):
+            if(obstacle_y + j >= HEIGHT_SIZE): break
+            for k in range(obstacle_width):
+                if(obstacle_x + k >= WIDTH_SIZE): break
+
+                if(obstacle_y + j < 3 and obstacle_x + k < 3): continue
+                if(obstacle_y + j > HEIGHT_SIZE-3 and obstacle_x + k > WIDTH_SIZE-3): continue
+                map_array[obstacle_y + j][obstacle_x + k] = 9
+
+    ex.updateMap(map_array)
     
     sys.exit(app.exec_())
